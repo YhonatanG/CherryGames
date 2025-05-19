a0e5657b7712e450c58abdf4d215f4410083fb6a
YhonatanG <ygarciab2@ucentral.edu.co>
2025-05-19 14:06:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from flask import Flask
 from data.load_csv import cargar_csv
+import os
 
-if __name__ == "__main__":
-    ruta_csv = "data/games.csv"  # Asegúrate de que esta ruta sea correcta
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ El servidor está funcionando correctamente."
+
+if __name__ == '__main__':
+    # Ruta al CSV
+    ruta_csv = 'data/juegos.csv'
+
+    # Cargar datos del CSV si la colección está vacía
     cargar_csv(ruta_csv)
+
+    # Puerto requerido por Render
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
